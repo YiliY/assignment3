b12c51c1776b939c2b64f5c7e86b67dfe482d6fc
Ryan Weber <rjw366@nyu.edu>
2015-09-25 07:50:07 -0400

    Assignment 3 single commit

---
+++ rjw366/my_package/hello_world.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 23, 2015
+
+@author: rjw366
+
+'''
+
+def hello_world():
+    print "Hello World!"
+
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    hello_world()
+    
+

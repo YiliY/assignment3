69bc030e414ba5a51269dae431dc20922b71bbf2
lt911 <lt911@nyu.edu>
2015-09-24 20:34:19 -0400

    homework completion

---
+++ lt911/my_package/hello_world.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 24, 2015
+
+@author: ds-ga-1007
+'''
+#
+# Python hello world
+#
+def hello_world():
+    print "Hello World!"
+
+if __name__ == "__main__":
+    import sys
+#    print "Hello World!"
+    hello_world()

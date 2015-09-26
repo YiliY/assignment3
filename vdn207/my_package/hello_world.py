0f8c82a380292f8c2483b2def329c3d87ef478b6
vdn207 <vdn207@nyu.edu>
2015-09-26 15:26:12 -0400

    Pushing Assignment 3 from Eclipse (PyDev).

---
+++ vdn207/my_package/hello_world.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 26, 2015
+
+@author: varundn
+
+'''
+
+def hello_world():
+    return "Hello World!"
+
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    print hello_world()

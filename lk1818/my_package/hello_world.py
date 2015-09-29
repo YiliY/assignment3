89183716515961384126879ebec6880f69af5838
Li Ke <lk1818@nyu.edu>
2015-09-28 21:35:06 -0400

    Assignment 3 Final Commit

---
+++ lk1818/my_package/hello_world.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 28, 2015
+
+@author: lk1818
+'''
+
+def hello_world():
+    return "Hello World!"
+
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    print hello_world()

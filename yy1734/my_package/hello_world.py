89867b1941f1db75620ef2663e9d29c93b6df85f
Yili Yu <yyyy@nyu.edu>
2015-10-19 15:20:19 -0400

    Assignment 3 added

---
+++ yy1734/my_package/hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Python hello world
+#
+
+#!/usr/bin/env python
+# Python hello world
+#
+def hello_world():
+    print "Hello World!"
+    
+if __name__ == "__main__":
+    import sys
+    print "Hello World!"
